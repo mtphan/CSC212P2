9f6feceed6b539c932ae959ccf48e9c7efbce468
Phuong M. Phan <mtphan@smith.edu>
2019-02-07 14:11:23 -0500

    I did some of the lab things.

--- src/main/java/edu/smith/cs/csc212/p2/Fish.java
+++ src/main/java/edu/smith/cs/csc212/p2/Fish.java
@@ -18,8 +18,10 @@ public class Fish extends WorldObject {
 	public static Color[] COLORS = {
 			Color.red,
 			Color.green,
-			Color.yellow
-			// TODO: (lab) Add more colors.
+			Color.yellow,
+			Color.blue,
+			Color.magenta,
+			Color.orange
 			// TODO: (P2) Maybe make a special fish that is more points?
 	};
 	/**
--- src/main/java/edu/smith/cs/csc212/p2/Rock.java
+++ src/main/java/edu/smith/cs/csc212/p2/Rock.java
@@ -27,7 +27,10 @@ public class Rock extends WorldObject {
 			new Color(72,60,50)
 	};
 	
-	// TODO(lab): introduce a member here that indexes the ROCK_COLORS array.
+	/**
+	 * Index of the color for rock.
+	 */
+	int color;
 	
 	/**
 	 * Construct a Rock in our world.
@@ -35,7 +38,8 @@ public class Rock extends WorldObject {
 	 */
 	public Rock(World world) {
 		super(world);
-		// TODO(lab): initialize your rock color index to a random number!
+		// Initialize color.
+		color = rand.nextInt(ROCK_COLORS.length);
 		// Note that all WorldObjects have a ``rand`` available so you don't need to make one.
 	}
 
@@ -44,8 +48,7 @@ public class Rock extends WorldObject {
 	 */
 	@Override
 	public void draw(Graphics2D g) {
-		// TODO(lab): use the right color in here...
-		g.setColor(Color.gray);
+		g.setColor(this.ROCK_COLORS[color]);
 		RoundRectangle2D rock = new RoundRectangle2D.Double(-.5,-.5,1,1,0.3,0.3);
 		g.fill(rock);
 	}
